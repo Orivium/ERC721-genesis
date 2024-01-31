33ef56b03e3f60d37dd5359a2913d03002171991
Paco Duhard-Jourdan <pduhardjourdan@wanadev.fr>
2024-01-31 13:04:36 +0100

    Create Testable Kings contracts with mint and burn public methods

---
+++ contracts/test/Testable_King2d.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import { King2d } from "../token/ERC721/King2d.sol";
+
+contract Testable_King2d is King2d {
+    constructor() King2d("0x1", "0x2", block.timestamp, _msgSender()) {}
+
+    function mint(address _to, uint256 _tokenId) external {
+        _mint(_to, _tokenId);
+    }
+
+    function burn(uint256 _tokenId) external {
+        _burn(_tokenId);
+    }
+}
