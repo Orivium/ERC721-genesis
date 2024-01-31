ac7cee7dc0ad1771b42b1afd16b0b316f05ff396
Paco Duhard-Jourdan <pduhardjourdan@wanadev.fr>
2024-01-31 13:04:36 +0100

    Create Testable Kings contracts with mint and burn public methods

---
+++ deploy/test/Testable_King2d.ts
@@ -0,0 +1,21 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const deployFunction: DeployFunction = async({ getNamedAccounts, deployments }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    if (!deployer) throw new Error("missing deployer");
+
+    await deploy("Testable_King2d", {
+        from: deployer,
+        args: [],
+        log: true,
+    });
+};
+
+export default deployFunction;
+deployFunction.tags = ['test', 'Testable_King2d'];
+
