0105bb2ff550a3c34775cec78e8ac8bb74eb6d7b
Pduhard <pacodj@free.fr>
2024-02-03 15:17:28 +0100

    Deploy on sepolia with staging api for testing

--- deploy/King.ts
+++ deploy/King.ts
@@ -2,54 +2,58 @@ import { DeployFunction } from "hardhat-deploy/types";
 import dotenv from "dotenv";
 
 import { MerkleTree } from "merkletreejs";
-import { getOGWhiteListedAddress, getWhiteListedAddress } from "../utils/whitelist";
+import {
+  getOGWhiteListedAddress,
+  getWhiteListedAddress,
+} from "../utils/whitelist";
 import { multiSigWallets, isSupportedNetwork } from "../utils/multiSigWallet";
 
 dotenv.config();
 
-const deployFunction: DeployFunction = async({ getNamedAccounts, deployments, network, ethers, run }) => {
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
+const deployFunction: DeployFunction = async ({
+  getNamedAccounts,
+  deployments,
+  network,
+  ethers,
+  run,
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-    if (!deployer) throw new Error("missing deployer");
-    if (!isSupportedNetwork(network.name)) throw new Error("unsupported network");
+  if (!deployer) throw new Error("missing deployer");
+  if (!isSupportedNetwork(network.name)) throw new Error("unsupported network");
 
-    const whitelist = await getWhiteListedAddress();
-    const wlLeafs = whitelist.map((address) => ethers.keccak256(address));
-    const wlMerkleTree = new MerkleTree(wlLeafs, ethers.keccak256, { sortPairs: true });
-    const wlRootHash = wlMerkleTree.getHexRoot();
+  const whitelist = await getWhiteListedAddress();
+  const wlLeafs = whitelist.map((address) => ethers.keccak256(address));
+  const wlMerkleTree = new MerkleTree(wlLeafs, ethers.keccak256, {
+    sortPairs: true,
+  });
+  const wlRootHash = wlMerkleTree.getHexRoot();
 
-    const ogWhitelist = await getOGWhiteListedAddress();
-    const ogLeafs = ogWhitelist.map((address) => ethers.keccak256(address));
-    const ogMerkleTree = new MerkleTree(ogLeafs, ethers.keccak256, { sortPairs: true });
-    const ogRootHash = ogMerkleTree.getHexRoot();
+  const ogWhitelist = await getOGWhiteListedAddress();
+  const ogLeafs = ogWhitelist.map((address) => ethers.keccak256(address));
+  const ogMerkleTree = new MerkleTree(ogLeafs, ethers.keccak256, {
+    sortPairs: true,
+  });
+  const ogRootHash = ogMerkleTree.getHexRoot();
 
-    const multiSigWallet = multiSigWallets[network.name] ?? deployer;
+  const multiSigWallet = multiSigWallets[network.name] ?? deployer;
 
-    await deploy("King", {
-        from: deployer,
-        args: [
-            wlRootHash,
-            ogRootHash,
-            multiSigWallet,
-        ],
-        log: true,
-        waitConfirmations: network.name === "hardhat" ? 0 : 5,
-    });
+  await deploy("King", {
+    from: deployer,
+    args: [wlRootHash, ogRootHash, multiSigWallet],
+    log: true,
+    waitConfirmations: network.name === "hardhat" ? 0 : 5,
+  });
 
-    if (network.name === "hardhat") return;
+  if (network.name === "hardhat") return;
 
-    await run("verify:verify", {
-        address: (await deployments.get("King")).address,
-        contract: "contracts/token/ERC721/King.sol:King",
-        constructorArguments: [
-            wlRootHash,
-            ogRootHash,
-            multiSigWallet,
-        ],
-    });
+  await run("verify:verify", {
+    address: (await deployments.get("King")).address,
+    contract: "contracts/token/ERC721/King.sol:King",
+    constructorArguments: [wlRootHash, ogRootHash, multiSigWallet],
+  });
 };
 
 export default deployFunction;
-deployFunction.tags = ['all', 'nft-genesis', 'King', 'main'];
-
+deployFunction.tags = ["all", "nft-genesis", "King", "main"];
